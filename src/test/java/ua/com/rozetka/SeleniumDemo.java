e9735f98b684e49728b7577496973b73b4ddab43
Yarmak Tanya <yarmaktatyana82@gmail.com>
2022-11-30 14:51:56 +0200

    adding new change

--- src/test/java/ua/com/rozetka/SeleniumDemo.java
+++ src/test/java/ua/com/rozetka/SeleniumDemo.java
@@ -2,13 +2,17 @@ package ua.com.rozetka;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
 
 public class SeleniumDemo {
 
-public static void main(String[] args){
-    System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Desktop\\chromedriver.exe");
-    WebDriver driver = new ChromeDriver();
-    driver.get("https://rozetka.com.ua/ua/");
-    driver.quit();
-}
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://rozetka.com.ua/ua/");
+        driver.quit();
+    }
 }
