a8273ae53d35c599e1e023184c2847d7eab846f1
Liubomyr Halamaha <galamagal86@gmail.com>
2022-12-01 16:08:24 +0200

    fix

--- src/test/java/ua/com/rozetka/SeleniumDemo.java
+++ src/test/java/ua/com/rozetka/SeleniumDemo.java
@@ -4,33 +4,40 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
+
 import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
 
 public class SeleniumDemo {
     WebDriver driver;
 
-    public  static  void main(String[] args) {
-
-
-    chromedriver().setup();
+    @Test
+    public void first() {
+        chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://rozetka.com.ua/ua/");
         WebElement userIcon = driver.findElement(By.xpath("/html/body/app-root/div/div/rz-header/rz-main-header/header/div/div/ul/li[3]/rz-user/button"));
         userIcon.click();
-         WebElement password  = driver.findElement(By.xpath("//*[@id=\"auth_pass\"]"));
-         password.sendKeys("pass");
-WebElement loginBtn = driver.findElement(By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[5]/button[1]"));
-loginBtn.click();
-WebElement errorMes = driver.findElement(By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[1]/p"));
-System.out.println("Message: " + errorMes.getText());
-
-        }
+        WebElement password = driver.findElement(By.xpath("//*[@id=\"auth_pass\"]"));
+        password.sendKeys("pass");
+        WebElement loginBtn = driver.findElement(By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[5]/button[1]"));
+        loginBtn.click();
+        WebElement errorMes = driver.findElement(By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[1]/p"));
+        System.out.println("Message: " + errorMes.getText());
 
     }
+
+    @AfterSuite
+    public void afterSuite() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
 
