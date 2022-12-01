8dafba0305cba7a13cf26d37b0d7c277642e34c3
Yarmak Tanya <yarmaktatyana82@gmail.com>
2022-12-01 15:31:48 +0200

    new file

--- src/test/java/ua/com/rozetka/SeleniumDemo.java
+++ src/test/java/ua/com/rozetka/SeleniumDemo.java
@@ -1,18 +1,36 @@
 package ua.com.rozetka;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.time.Duration;
 
+
+
+import java.time.Duration;
+import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
 
 public class SeleniumDemo {
+    WebDriver driver;
 
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\chromedriver.exe");
+    public  static  void main(String[] args) {
+
+
+    chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://rozetka.com.ua/ua/");
-        driver.quit();
+        WebElement userIcon = driver.findElement(By.xpath("/html/body/app-root/div/div/rz-header/rz-main-header/header/div/div/ul/li[3]/rz-user/button"));
+        userIcon.click();
+         WebElement password  = driver.findElement(By.xpath("//*[@id=\"auth_pass\"]"));
+         password.sendKeys("pass");
+WebElement loginBtn = driver.findElement(By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[5]/button[1]"));
+loginBtn.click();
+WebElement errorMes = driver.findElement(By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[1]/p"));
+System.out.println("Message: " + errorMes.getText());
+
+        }
+
     }
-}
+
